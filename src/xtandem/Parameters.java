0fc4fbb4981996051d885a179a2146cb787128d0
Thilo.Muth@uni-jena.de <Thilo.Muth@uni-jena.de@18bb6550-a433-11de-a590-a9c80ee327d0>
2009-09-24 15:55:05 +0000

    - new parameters class with constants

---
+++ src/xtandem/Parameters.java
@@ -0,0 +1,15 @@
+package xtandem;
+
+/**
+ * This class holds all the parameters needed for the X!Tandem file processing.
+ * @author Thilo Muth
+ *
+ */
+public class Parameters {
+	
+	/**
+	 * The fragment mass error used in the matching of theoretical and experimental spectra.
+	 */
+	public final static double FRAGMENTMASSERROR = 0.3;	
+
+}
