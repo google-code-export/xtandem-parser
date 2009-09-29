66dbe6623b3406db5ee0c49a5ce0ae121e03acb5
Thilo.Muth@uni-jena.de <Thilo.Muth@uni-jena.de@18bb6550-a433-11de-a590-a9c80ee327d0>
2009-09-29 13:55:07 +0000

    - api documentation

--- src/xtandem/Protein.java
+++ src/xtandem/Protein.java
@@ -33,7 +33,15 @@ public class Protein implements Serializable{
 	 * This variable presents the sum of all the fragment ions that identify this protein.
 	 */
 	private double iSummedScore = 0;
-
+	
+	/**
+	 * The constructor get id, uid, label, expect value and summed score to build a protein object.
+	 * @param aID
+	 * @param aUID
+	 * @param aLabel
+	 * @param aExpectValue
+	 * @param aSummedScore
+	 */
 	public Protein(String aID, String aUID, String aLabel, double aExpectValue, double aSummedScore){
 		iID = aID;
 		iUID = aUID;
@@ -42,42 +50,82 @@ public class Protein implements Serializable{
 		iSummedScore = aSummedScore;
 	}
 
+	/**
+	 * Returns the expect value for the protein.
+	 * @return iExpectValue
+	 */
 	public double getExpectValue() {
 		return iExpectValue;
 	}
 
-	public void setExpectValue(double expectValue) {
-		iExpectValue = expectValue;
+	/**
+	 * Sets the expect value for the protein.
+	 * @param aExpectValue
+	 */
+	public void setExpectValue(double aExpectValue) {
+		iExpectValue = aExpectValue;
 	}
 
+	/**
+	 * Returns the id of the protein.
+	 * @return iID
+	 */
 	public String getID() {
 		return iID;
 	}
-
+	
+	/**
+	 * Sets the id of the protein.
+	 * @param aId
+	 */
 	public void setID(String aId) {
 		iID = aId;
 	}
-
+	
+	/**
+	 * Returns the uId of the protein.
+	 * @return iUID
+	 */
 	public String getUID() {
 		return iUID;
 	}
-
+	
+	/**
+	 * Sets the uID of the protein.
+	 * @param aUid
+	 */
 	public void setUID(String aUid) {
 		iUID = aUid;
 	}
 
+	/**
+	 * Returns the label of the protein.
+	 * @return iLabel
+	 */
 	public String getLabel() {
 		return iLabel;
 	}
-
+	
+	/**
+	 * Sets the label of the protein.
+	 * @param aLabel
+	 */
 	public void setLabel(String aLabel) {
 		iLabel = aLabel;
 	}
 
+	/**
+	 * Returns the summed score of the protein.
+	 * @return iSummedScore
+	 */
 	public double getSummedScore() {
 		return iSummedScore;
 	}
-
+	
+	/**
+	 * Sets the summed score of the protein.
+	 * @param aSummedScore
+	 */
 	public void setSummedScore(double aSummedScore) {
 		iSummedScore = aSummedScore;
 	}
