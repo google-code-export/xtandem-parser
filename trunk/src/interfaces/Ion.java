c3df73ccae716fd81d283843ed44641f88b49d26
Thilo.Muth@uni-jena.de <Thilo.Muth@uni-jena.de@18bb6550-a433-11de-a590-a9c80ee327d0>
2009-09-29 13:02:41 +0000

    - added api documentation

--- trunk/src/interfaces/Ion.java
+++ trunk/src/interfaces/Ion.java
@@ -11,23 +11,66 @@ package interfaces;
  */
 public interface Ion {    
 
+	/**
+	 * This int is the identifier for an a ion.
+	 */
 	public final static int A_ION = 0;
+	/**
+	 * This int is the identifier for a b ion.
+	 */
 	public final static int B_ION = 1;
+	/**
+	 * This int is the identifier for a c ion.
+	 */
 	public final static int C_ION = 2;
+	/**
+	 * This int is the identifier for a x ion.
+	 */
 	public final static int X_ION = 3;
+	/**
+	 * This int is the identifier for a y ion.
+	 */
 	public final static int Y_ION = 4;
+	/**
+	 * This int is the identifier for a z ion.
+	 */
 	public final static int Z_ION = 5;
-
+	/**
+	 * This method checks whether the ion matched with a given set of peaks
+	 * @param peaks
+	 * @param aMassError
+	 * @return boolean
+	 */
 	public boolean isMatch(Peak[] peaks, double aMassError);
 	
+	/**
+	 * Returns the m/z.	 
+	 * @return double
+	 */
     public double getMZ();
    
+    /**
+     * Returns the intensity.
+     * @return double
+     */
     public double getIntensity();
     
+    /**
+     * Returns the type.
+     * @return String
+     */
     public String getType();
     
+    /**
+     * Returns the ion number.
+     * @return int
+     */
     public int getNumber();
 
+    /**
+     * Returns the score.
+     * @return double
+     */
     public double getScore();
 
 }
